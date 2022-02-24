8aaf1a892b197e1f54524ce2faa7cf8a1e69d155
jbarlas <77653620+jbarlas@users.noreply.github.com>
2022-02-24 15:23:04 -0500

    added heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const players = require('./routes/api/players');
 const teams = require('./routes/api/teams');
@@ -23,6 +24,16 @@ mongoose
 // use routes 
 app.use('/api/players', players);
 app.use('/api/teams', teams)
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder 
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build',  'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
